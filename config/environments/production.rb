f4450ed2cb4f8e7e42ad5de6b5ca0e9cc1eddec4
poc7667 <poc7667@gmail.com>
2014-12-10 09:47:01 +0800

    Limit log size

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # limit log size
+  config.logger = Logger.new(config.paths.log.first, 10, 100.megabytes)
 end
