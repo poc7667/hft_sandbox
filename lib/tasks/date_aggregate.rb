7ac9f0e5371b1f0f67a972e2463f75f5c5d55311
poc7667 <poc7667@gmail.com>
2014-12-19 17:32:35 +0800

    Update data aggregated function

--- lib/tasks/date_aggregate.rb
+++ lib/tasks/date_aggregate.rb
@@ -37,23 +37,21 @@ module DateAggregate
   end
 
 
-  def get_query_command(interval, contract_month, product_type='SR', market='czces')
+  def get_query_command(frequence, contract_month, product_type='SR', market='czces')
     %{
       SELECT DISTINCT ON (1)
-           date_trunc('#{interval}', ticktime) AS ticktime_stamp
+           date_trunc('#{frequence}', ticktime) AS ticktime
          , first_value(last_price) OVER w AS open
          , max(last_price)         OVER w AS high
          , min(last_price)         OVER w AS low
          , last_value(last_price)  OVER w AS close
-         , sum(last_volume)        OVER w AS tt_volume
+         , sum(last_volume)        OVER w AS volume
          , product_type
          , contract_month
         FROM   #{market}
-        WHERE  ticktime >= '#{begin_time}'::timestamp
-        AND    ticktime <  '#{end_time}'::timestamp
-        AND    product_type ='#{product_type}'
+        WHERE  product_type ='#{product_type}'
         AND    contract_month = '#{contract_month}'::timestamp
-        WINDOW w AS (PARTITION BY date_trunc('#{interval}', ticktime) ORDER BY ticktime
+        WINDOW w AS (PARTITION BY date_trunc('#{frequence}', ticktime) ORDER BY ticktime
                      ROWS BETWEEN UNBOUNDED PRECEDING
                               AND UNBOUNDED FOLLOWING)
         ORDER  BY 1
