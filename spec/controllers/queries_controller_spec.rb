9115b24a46da5a10ca94bd7ce7fb60d8a7f03d8f
poc7667 <poc7667@gmail.com>
2014-12-27 16:30:26 +0800

    Add more query examples

--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -12,9 +12,25 @@ RSpec.describe QueriesController, :type => :controller do
   let(:valid_session) { {} }
 
   describe "Get index" do
-    # it "simple query" do
-    #   get :index, {sample_freq: "day", begin_time: Date.today}
-    # end
+
+
+#    contract_month    | product_type
+# ---------------------+--------------
+#  2014-01-01 00:00:00 | IF
+#  2014-02-01 00:00:00 | IF
+#  2014-03-01 00:00:00 | IF
+#  2014-03-01 00:00:00 | TF
+#  2014-04-01 00:00:00 | IF
+#  2014-05-01 00:00:00 | IF
+#  2014-06-01 00:00:00 | IF
+#  2014-06-01 00:00:00 | TF
+#  2014-07-01 00:00:00 | IF
+#  2014-09-01 00:00:00 | IF
+#  2014-09-01 00:00:00 | TF
+#  2014-12-01 00:00:00 | IF
+#  2014-12-01 00:00:00 | TF
+
+
 
     it "with start_dt and end_dt, frequency: second" do
       get :index, {
@@ -26,6 +42,17 @@ RSpec.describe QueriesController, :type => :controller do
         end_dt: "2014-01-20 06:16:00"
       }
     end
+
+    it "with start_dt and end_dt, frequency: second" do
+      get :index, {
+        market: "cffex_hfts",
+        product: "TF",
+        frequency: "second",
+        contract_month: "2014-03-01 00:00:00",
+        start_dt: "2014-01-20 05:26:00",
+        end_dt: "2014-01-20 06:16:00"
+      }
+    end    
 
     it "with start_dt and end_dt, frequency: hour" do
       get :index, {
@@ -54,7 +81,7 @@ RSpec.describe QueriesController, :type => :controller do
         market: "cffex_hfts",
         product: "IF",
         frequency: "hour",
-        contract_month: "2014-03-01 00:00:00",
+        contract_month: "2014-12-01 00:00:00",
         event_dt: "2014-01-10 01:26:00",
         delta_time: "10day"
       }
@@ -70,6 +97,64 @@ RSpec.describe QueriesController, :type => :controller do
         delta_time: "5hour"
       }
     end
+
+    it "delta 17 hour, frequence:minute" do
+      get :index, {
+        market: "czce_hfts",
+        product: "FG",
+        frequency: "minute",
+        contract_month: "2014-03-01 00:00:00",
+        event_dt: "2014-01-13 01:00:00",
+        delta_time: "17hour"
+      }
+    end
+
+    it "delta 17 day, frequence:hour" do
+      get :index, {
+        market: "czce_hfts",
+        product: "SR",
+        frequency: "hour",
+        contract_month: "2014-03-01 00:00:00",
+        event_dt: "2014-01-13 01:00:00",
+        delta_time: "17day"
+      }
+    end    
+
+    it "delta 5 month, frequence:hour" do
+      get :index, {
+        market: "czce_hfts",
+        product: "SR",
+        frequency: "hour",
+        contract_month: "2014-11-01 00:00:00",
+        event_dt: "2014-01-13 01:00:00",
+        delta_time: "5month"
+      }
+    end    
+
+
+#    contract_month    | product_type
+# ---------------------+--------------
+#  2014-02-01 00:00:00 | FG
+#  2014-02-01 00:00:00 | TC
+#  2014-03-01 00:00:00 | OI
+#  2014-03-01 00:00:00 | RM
+#  2014-03-01 00:00:00 | SR
+#  2014-04-01 00:00:00 | ME
+#  2014-06-01 00:00:00 | TA
+#  2014-08-01 00:00:00 | TA
+#  2014-09-01 00:00:00 | OI
+#  2014-09-01 00:00:00 | RS
+#  2014-09-01 00:00:00 | WH
+#  2014-11-01 00:00:00 | CF
+#  2014-11-01 00:00:00 | PM
+#  2014-12-01 00:00:00 | TA
+#  2015-01-01 00:00:00 | CF
+#  2015-01-01 00:00:00 | PM
+#  2015-02-01 00:00:00 | TA
+#  2015-03-01 00:00:00 | ME
+#  2015-05-01 00:00:00 | FG
+#  2015-05-01 00:00:00 | ME
+#  2015-11-01 00:00:00 | SR
 
 
   end
