a168f8c28c1df3b525d08529eab4b5102900a5be
poc7667 <poc7667@gmail.com>
2014-12-27 16:03:14 +0800

    Refactor: query interface

--- app/controllers/concerns/queryable.rb
+++ app/controllers/concerns/queryable.rb
@@ -5,29 +5,13 @@ module Queryable
   attr_accessor :errors , :query_type , :q
   errors = []
 
-  def build_query
-    %{
-      SELECT DISTINCT ON (time)
-        time, high, low, open, close, volume
-        FROM #{q[:market]} WHERE product_type = '#{q[:product]}'
-        AND frequence = '#{q[:frequency]}'
-        AND contract_month = '#{q[:contract_month]}'
-        AND time >= '#{q[:start_dt]}'
-        AND time < '#{q[:end_dt]}' 
-    }.gsub(/\s+/, " ").strip
-  end
-
   def get_query_result
     filter_params
     check_params
     normalize_params
-    q_cmd = build_query
-    print(URI::unescape(q.to_query))
-    res = []
-    # ActiveRecord::Base.connection.execute(q_cmd).each do |raw_record|
-    #   res << raw_record.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-    # end
-  end  
+    print_query_log
+    ActiveRecord::Base.connection.execute(build_query)
+  end
 
   def filter_params
     self.q = params.clone
@@ -49,13 +33,30 @@ module Queryable
   end
 
   def normalize_params
-    if :specified == query_type
-      return specified_query
-    elsif :relative == query_type
+    if :relative == query_type
       delta_time = get_delta_time
       q[:start_dt] = q[:event_dt]
       q[:end_dt] = (Date.parse(q[:start_dt]) + eval(delta_time)).strftime('%Y-%m-%d %H:%M:%S')
+      q.delete(:event_dt)
+      q.delete(:delta_time)
     end
+  end
+
+  def print_query_log
+    ap("http://localhost:3000/?"+URI::unescape(q.to_query))
+  end
+
+
+  def build_query
+    %{
+      SELECT DISTINCT ON (time)
+        time, high, low, open, close, volume
+        FROM #{q[:market]} WHERE product_type = '#{q[:product]}'
+        AND frequence = '#{q[:frequency]}'
+        AND contract_month = '#{q[:contract_month]}'
+        AND time >= '#{q[:start_dt]}'
+        AND time < '#{q[:end_dt]}' 
+    }.gsub(/\s+/, " ").strip
   end
 
 end
